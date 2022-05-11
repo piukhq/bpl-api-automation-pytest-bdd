7052ef8967dd10889d9027fccbb48d0d3d27031e
RupalPatelj <jitindia2003@yahoo.com>
2022-05-11 11:51:07 +0100

    Added randint

--- tests/rewards_rule_management_api/step_defs/transaction_steps.py
+++ tests/rewards_rule_management_api/step_defs/transaction_steps.py
@@ -45,7 +45,7 @@ def send_transaction_request(payload_type: str, retailer_slug: str, token: str, 
             "datetime": int(datetime.utcnow().timestamp()),
             "MID": "12432432",
             "loyalty_id": str(account_holder_uuid),
-            "transaction_id": "BPL" + str(random.randint(1, (10**10) - 1)).zfill(10),
+            "transaction_id": "BPL" + str(random.randint(1, (10 ** 10))),
         }
 
     if payload_type == "over the threshold":
